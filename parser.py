fb245558c905c35b7e7acbd5d25c1a0cc8f174f5
12rics <jwp1994@naver.com>
2019-02-25 22:41:30 +0900

    fixed parser to merge mascara and brush json files

--- parser.py
+++ parser.py
@@ -1,10 +1,12 @@
 import json
 
-annotations = json.load(open("via_region_data.json"))
-annotations = list(annotations.values())
+mascara = json.load(open("via_region_data_mascara.json"))
+mascara = list(mascara.values())
+brush = json.load(open("via_region_data_brush.json"))
+brush = list(brush.values())
 
 f=open("train.txt","w")
-for i, a in enumerate(annotations):
+for i, a in enumerate(mascara):
     f.write('images/' + a['filename'])
     f.write(" ")
     b = list(a['regions'].values())
@@ -21,6 +23,22 @@ for i, a in enumerate(annotations):
         f.write("0")
         if j < len(b) - 1:
             f.write(" ")
-    if i < len(annotations) - 1:
+    if brush[i]['regions']:
+        brush_list = list(brush[i]['regions'].values())
+        for j, c in enumerate(brush_list):
+            d = c['shape_attributes']
+            f.write(" ")
+            f.write(str(d['x']))
+            f.write(",")
+            f.write(str(d['y']))
+            f.write(",")
+            f.write(str(d['x']+d['width']))
+            f.write(",")
+            f.write(str(d['y']+d['height']))
+            f.write(",")
+            f.write("1")
+            if j < len(b) - 1:
+                f.write(" ")
+    if i < len(mascara) - 1:
         f.write("\n")
 f.close()
