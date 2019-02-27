4dcc64b7e420f48dbd11b93d286215dd1e0a2a3d
robert kim <jrheonghwan@naver.com>
2019-02-27 20:05:19 +0900

    change epoch num

--- yolo.py
+++ yolo.py
@@ -142,9 +142,10 @@ class YOLO(object):
             area = (left, top, right - left, bottom - top)
             cropped_img = image.crop(area)
             origin = Image.open('images/image.jpg')
-            vector1 = image_parse(10, origin)
-            vector2 = image_parse(10, cropped_img)
-            if (compare_color(origin, cropped_img) or cosine_compare(vector1, vector2, 10)):
+            if predicted_class == 'bottle' :
+            # vector1 = image_parse(10, origin)
+            # vector2 = image_parse(10, cropped_img)
+            # if (compare_color(origin, cropped_img) or cosine_compare(vector1, vector2, 10)) and predicted_class == 'bottle':
                 label = '{} {:.2f}'.format(predicted_class, score)
                 draw = ImageDraw.Draw(image)
                 label_size = draw.textsize(label, font)
